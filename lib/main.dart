23bd9e42d6bbf30bc7202a691a81e3c4e6712f18
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-14 10:54:06 -0700

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools;
 
 void main() {
   runApp(const App());
@@ -46,61 +47,106 @@ class App extends StatelessWidget {
                                 width: 50,
                                 height: 50,
                                 color: Colors.orange,
+                                child: const Center(
+                                  child: Text('1'),
+                                ),
                               ),
                               child: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.orange,
+                                child: const Center(
+                                  child: Text('1'),
+                                ),
                               ),
                             ),
                             const SizedBox(width: 10.0),
                             Draggable(
-                              data: CaptchaOptions.one,
+                              data: CaptchaOptions.two,
                               feedback: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.redAccent,
+                                child: const Center(
+                                  child: Text('2'),
+                                ),
                               ),
                               child: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.redAccent,
+                                child: const Center(
+                                  child: Text('2'),
+                                ),
                               ),
                             ),
                             const SizedBox(width: 10.0),
                             Draggable(
-                              data: CaptchaOptions.one,
+                              data: CaptchaOptions.three,
                               feedback: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.greenAccent,
+                                child: const Center(
+                                  child: Text('3'),
+                                ),
                               ),
                               child: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.greenAccent,
+                                child: const Center(
+                                  child: Text('3'),
+                                ),
                               ),
                             ),
                             const SizedBox(width: 10.0),
                             Draggable(
-                              data: CaptchaOptions.one,
+                              data: CaptchaOptions.four,
                               feedback: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.blueAccent,
+                                child: const Center(
+                                  child: Text('4'),
+                                ),
                               ),
                               child: Container(
                                 width: 50,
                                 height: 50,
                                 color: Colors.blueAccent,
+                                child: const Center(
+                                  child: Text('4'),
+                                ),
                               ),
                             ),
                           ],
                         ),
-                        Container(
-                          width: 60,
-                          height: 60,
-                          color: Colors.white,
+                        DragTarget<CaptchaOptions>(
+                          onWillAccept: (data) {
+                            if (data == CaptchaOptions.one) {
+                              devtools.log('Accepted!');
+                              return true;
+                            }
+                            return false;
+                          },
+                          onAccept: (data) {
+                            // When target is released, do something here
+                            devtools.log('Accepted: $data');
+                          },
+                          onAcceptWithDetails: (details) {
+                            // When target is released, do something here
+                            devtools.log('onAcceptWithDetails: $details');
+                          },
+                          builder: (context, candidateData, rejectedData) {
+                            // devtools.log('candidateData: $candidateData');
+                            // devtools.log('rejectedData: $rejectedData');
+                            return Container(
+                              width: 60,
+                              height: 60,
+                              color: Colors.white,
+                            );
+                          },
                         ),
                       ],
                     ),
