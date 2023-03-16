ec39eae139245e9b3e5c65ef2038031faa3267b9
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-16 14:29:33 -0700

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools;
 
@@ -17,40 +19,53 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(),
-        body: LayoutBuilder(
-          builder: (_, constraints) {
-            final width = constraints.maxWidth;
-            final height = constraints.maxHeight;
+      home: const CaptchaScreen(),
+    );
+  }
+}
 
-            return SizedBox(
-              height: height,
-              width: width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: width * .8,
-                    height: 140,
-                    color: Colors.grey,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          children: [
-                            Draggable(
-                              data: CaptchaOptions.one,
-                              feedback: Container(
-                                width: 50,
-                                height: 50,
-                                color: Colors.orange,
-                                child: const Center(
-                                  child: Text('1'),
-                                ),
-                              ),
+class CaptchaScreen extends StatefulWidget {
+  const CaptchaScreen({super.key});
+
+  @override
+  State<CaptchaScreen> createState() => _CaptchaScreenState();
+}
+
+class _CaptchaScreenState extends State<CaptchaScreen> {
+  final _authenticatedNotifier = ValueNotifier(false);
+  final targetCaptcha = CaptchaOptions.values.elementAt(
+    Random().nextInt(4),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(),
+      body: LayoutBuilder(
+        builder: (_, constraints) {
+          final width = constraints.maxWidth;
+          final height = constraints.maxHeight;
+
+          return SizedBox(
+            height: height,
+            width: width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: width * .8,
+                  height: 140,
+                  color: Colors.grey,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: [
+                          Draggable(
+                            data: CaptchaOptions.one,
+                            feedback: Material(
                               child: Container(
                                 width: 50,
                                 height: 50,
@@ -60,17 +75,19 @@ class App extends StatelessWidget {
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 10.0),
-                            Draggable(
-                              data: CaptchaOptions.two,
-                              feedback: Container(
-                                width: 50,
-                                height: 50,
-                                color: Colors.redAccent,
-                                child: const Center(
-                                  child: Text('2'),
-                                ),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.orange,
+                              child: const Center(
+                                child: Text('1'),
                               ),
+                            ),
+                          ),
+                          const SizedBox(width: 10.0),
+                          Draggable(
+                            data: CaptchaOptions.two,
+                            feedback: Material(
                               child: Container(
                                 width: 50,
                                 height: 50,
@@ -80,17 +97,19 @@ class App extends StatelessWidget {
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 10.0),
-                            Draggable(
-                              data: CaptchaOptions.three,
-                              feedback: Container(
-                                width: 50,
-                                height: 50,
-                                color: Colors.greenAccent,
-                                child: const Center(
-                                  child: Text('3'),
-                                ),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.redAccent,
+                              child: const Center(
+                                child: Text('2'),
                               ),
+                            ),
+                          ),
+                          const SizedBox(width: 10.0),
+                          Draggable(
+                            data: CaptchaOptions.three,
+                            feedback: Material(
                               child: Container(
                                 width: 50,
                                 height: 50,
@@ -100,17 +119,19 @@ class App extends StatelessWidget {
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 10.0),
-                            Draggable(
-                              data: CaptchaOptions.four,
-                              feedback: Container(
-                                width: 50,
-                                height: 50,
-                                color: Colors.blueAccent,
-                                child: const Center(
-                                  child: Text('4'),
-                                ),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.greenAccent,
+                              child: const Center(
+                                child: Text('3'),
                               ),
+                            ),
+                          ),
+                          const SizedBox(width: 10.0),
+                          Draggable(
+                            data: CaptchaOptions.four,
+                            feedback: Material(
                               child: Container(
                                 width: 50,
                                 height: 50,
@@ -120,42 +141,100 @@ class App extends StatelessWidget {
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                        DragTarget<CaptchaOptions>(
-                          onWillAccept: (data) {
-                            if (data == CaptchaOptions.one) {
-                              devtools.log('Accepted!');
-                              return true;
-                            }
-                            return false;
-                          },
-                          onAccept: (data) {
-                            // When target is released, do something here
-                            devtools.log('Accepted: $data');
-                          },
-                          onAcceptWithDetails: (details) {
-                            // When target is released, do something here
-                            devtools.log('onAcceptWithDetails: $details');
-                          },
-                          builder: (context, candidateData, rejectedData) {
-                            // devtools.log('candidateData: $candidateData');
-                            // devtools.log('rejectedData: $rejectedData');
-                            return Container(
-                              width: 60,
-                              height: 60,
-                              color: Colors.white,
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.blueAccent,
+                              child: const Center(
+                                child: Text('4'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ValueListenableBuilder(
+                        valueListenable: _authenticatedNotifier,
+                        builder: (_, bool authenticated, __) {
+                          if (authenticated) return SizedBox.shrink();
+
+                          return DragTarget<CaptchaOptions>(
+                            onWillAccept: (data) {
+                              if (data == targetCaptcha) {
+                                devtools.log('Accepted!');
+                                return true;
+                              }
+                              return false;
+                            },
+                            onAccept: (data) {
+                              // When target is released, do something here
+                              devtools.log('Accepted: $data');
+                              _authenticatedNotifier.value = true;
+                            },
+                            onAcceptWithDetails: (details) {
+                              // When target is released, do something here
+                              devtools.log('onAcceptWithDetails: $details');
+                            },
+                            builder: (context, candidateData, rejectedData) {
+                              // devtools.log('candidateData: $candidateData');
+                              // devtools.log('rejectedData: $rejectedData');
+                              return Container(
+                                width: 60,
+                                height: 60,
+                                color: Colors.white,
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AuthenticatedTarget extends StatefulWidget {
+  const AuthenticatedTarget({super.key});
+
+  @override
+  State<AuthenticatedTarget> createState() => _AuthenticatedTargetState();
+}
+
+class _AuthenticatedTargetState extends State<AuthenticatedTarget> {
+  final _loadingNotifier = ValueNotifier(true);
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        await Future.delayed(const Duration(milliseconds: 1500));
+        _loadingNotifier.value = false;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      height: 60,
+      child: ValueListenableBuilder(
+        valueListenable: _loadingNotifier,
+        builder: (_, bool isLoading, __) {
+          if (isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
+          }
+
+          return SizedBox.shrink();
+        },
       ),
     );
   }
